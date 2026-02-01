3b142914dbfe24fbd8bf7e861dd67b6ac6454ee0
MarianaNamie <mariana.ribeiro@sptech.school>
2026-02-01 00:13:14 -0300

    criação simples de API

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+livros = [
+    {
+        'id': 1,
+        'titulo': 'O Senhor dos Anéis - A Sociedade do Anel',
+        'autor': 'J.R.R. Tolkien'
+    },
+    {
+        'id': 2,
+        'titulo': 'Harry Potter e a Pedra Filosofal',
+        'autor': 'J.K. Rowling'
+    },
+    {
+        'id': 3,
+        'titulo': 'Hábitos Atômicos',
+        'autor': 'James Clear'
+    },
+]
+
+# Consultar todos os livros
+@app.route('/livros', methods=['GET'])
+def obter_livros():
+    return jsonify(livros)
+
+
+# Consultar livro por ID
+@app.route('/livros/<int:id>', methods=['GET'])
+def obter_livro_por_id(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
+    return jsonify({'erro': 'Livro não encontrado'}), 404
+
+
+# Editar livro por ID
+@app.route('/livros/<int:id>', methods=['PUT'])
+def editar_livro_por_id(id):
+    livro_alterado = request.get_json()
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+    return jsonify({'erro': 'Livro não encontrado'}), 404
+
+
+# Criar novo livro
+@app.route('/livros', methods=['POST'])
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+    return jsonify(livros), 201
+
+
+# Excluir livro por ID
+@app.route('/livros/<int:id>', methods=['DELETE'])
+def excluir_livro(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+            return jsonify(livros)
+    return jsonify({'erro': 'Livro não encontrado'}), 404
+
+
+app.run(port=5000, host='localhost', debug=True)
